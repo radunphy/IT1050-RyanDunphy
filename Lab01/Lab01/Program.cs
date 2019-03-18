20f301df363a58afb0b48efe3c13fa68b3d5ce16
Ryan Dunphy <S01131630@acad.tri-c.edu>
2019-03-18 15:52:18 -0400

    Completed Lab 1

---
+++ Lab01/Lab01/Program.cs
@@ -0,0 +1,30 @@
+﻿// A Hello World! program in C#.
+/* The above is a comment, useful for understanding why
+ something was done or how it works. The asterisk on
+ this comment lets me write the comment across multiple
+ lines. */
+using System;
+//Allows direct use of System classes without fully qualifying them. 
+namespace HelloWorld
+{
+    class Hello
+    {
+        static void Main()
+        /* Every C# console application must have a Main
+         method, void is used to make Main return nothing.
+         Also could have used static int. to return an 
+         integer. */
+        /* With either of those return types, it can take
+         arguments. (static void Main(string[] args) or 
+         static int Main(string[] args) return 0;)*/
+        // args is a string array.
+        {
+            Console.WriteLine("Hello World from Ryan Dunphy!");
+            //One of the output methods of the console class.
+
+            // Keep the console window open in debug mode.
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
